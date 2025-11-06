10bf71b5e2f321acd7072c934cd2ef1c404e466a
Manco-Mendez Elvis-Neiser <elvis.manco@inetum.com>
2025-11-06 16:22:15 -0500

    project structure

---
+++ src/main/java/com/inetum/apisimulationloans/ApiSimulationLoansApplication.java
@@ -0,0 +1,13 @@
+package com.inetum.apisimulationloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSimulationLoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSimulationLoansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inetum/apisimulationloans/ApiSimulationLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inetum.apisimulationloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSimulationLoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
