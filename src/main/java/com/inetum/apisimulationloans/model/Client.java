8636c4b0b7b4f968eaff58f0434912f37390a94d
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2025-11-07 07:31:47 -0500

    project structure

--- src/main/java/com/inetum/apisimulationloans/model/Client.java
+++ src/main/java/com/inetum/apisimulationloans/model/Client.java
@@ -1,13 +1,19 @@
 package com.inetum.apisimulationloans.model;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "clients")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long clientId;
+
     private String firstName;
     private String paternalLastName;
     private String maternalLastName;
